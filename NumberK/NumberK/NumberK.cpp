cb50e6d2a3b004125b9675851d976b52c819146f
donghyun <gtapex8639@gmail.com>
2021-04-21 12:06:41 +0900

    initial setting

---
+++ NumberK/NumberK/NumberK.cpp
@@ -0,0 +1,26 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> array, vector<vector<int>> commands) {
+    vector<int> answer;
+
+    for (int i = 0; i < commands.size(); i++) {
+
+    }
+
+    return answer;
+}
+
+void main() {
+
+    int nums[] = {1, 5, 2, 6, 3, 7, 4};
+    vector<int> array;
+    for (int i = 0; i < 7; i++) {
+        array.push_back(nums[i]);
+    }
+
+    vector<int> commands = {2, 5, 3};
+
+}

2f9294aeaf953722caa58e00b0e6d1e00363b1c9
donghyun <gtapex8639@gmail.com>
2021-04-26 16:32:52 +0900

    resolve problem

--- NumberK/NumberK/NumberK.cpp
+++ NumberK/NumberK/NumberK.cpp
@@ -1,13 +1,39 @@
 #include <string>
 #include <vector>
+#include <iostream>
 
 using namespace std;
+
+vector<int> sort(vector<int> temp) {
+    vector<int> sorted = temp;
+    for (int i = 0; i < sorted.size(); i++) {
+        for (int j = i + 1; j < sorted.size(); j++) {
+            if (sorted[j] < sorted[i]) {
+                int temp = sorted[i];
+                sorted[i] = sorted[j];
+                sorted[j] = temp;
+            }
+        }
+    }
+
+    for (int k = 0; k < sorted.size(); k++) {
+        std::cout << sorted[k] << " ";
+    }
+    std::cout << "\n";
+
+    return sorted;
+}
 
 vector<int> solution(vector<int> array, vector<vector<int>> commands) {
     vector<int> answer;
 
     for (int i = 0; i < commands.size(); i++) {
-
+        vector<int> temp;
+        for (int j = commands[i][0] - 1; j < commands[i][1]; j++) {
+            temp.push_back(array[j]);
+        }
+        temp = sort(temp);
+        answer.push_back(temp[commands[i][2] - 1]);
     }
 
     return answer;
@@ -21,6 +47,9 @@ void main() {
         array.push_back(nums[i]);
     }
 
-    vector<int> commands = {2, 5, 3};
+    vector<vector<int>> commands = { {2, 5, 3} };
+    vector<int> answer = solution(array, commands);
+
+    std::cout << "answer : " << answer[0];
 
 }
